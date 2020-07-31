5c83e0d0ee8c438b621a7bacbdc09ff0f10a9c96
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-07-31 07:00:47 -0500

    commands for visual regression testing

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,12 +1,4 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-import { addMatchImageSnapshotPlugin } from "cypress-image-snapshot/plugin"
+const { addMatchImageSnapshotPlugin } = require("cypress-image-snapshot/plugin")
 module.exports = (on, config) => {
 	// `on` is used to hook into various events Cypress emits
 	// `config` is the resolved Cypress config
