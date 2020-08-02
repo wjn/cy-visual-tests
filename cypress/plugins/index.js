8468048880e0284292b43406fa60f97b026ea3b1
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-08-02 08:26:41 -0500

    removed previous integration tests, added percy test, and config to run with Percy

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,5 @@
 const { addMatchImageSnapshotPlugin } = require("cypress-image-snapshot/plugin")
-const percyHealthCheck = require("@percy/cypress")
+const percyHealthCheck = require("@percy/cypress/task")
 module.exports = (on, config) => {
 	// `on` is used to hook into various events Cypress emits
 	// `config` is the resolved Cypress config
